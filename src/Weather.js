f6f1219e8b5b1a4f11dbdb3f9af1f5932fb82724
Niharika Dutta <dniharika16@.com>
2021-01-15 15:27:52 +0530

    sec_update

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { fetchWeather } from './Api/fetchWeather';
 import './Weather.css';
 import Hat from "./hatred.svg";
@@ -13,8 +13,7 @@ function Weather() {
 
     if (e.key === 'Enter') {
       const data = await fetchWeather(val);
-      // console.log(data.dt);
-      console.log((new Date(data.dt * 1000 - (data.timezone * 1000))).toString());
+      // console.log(data);    
       const k = ((new Date(data.dt * 1000 - (data.timezone * 1000))).toString());
       setdate(k);
       setWeather(data);
@@ -27,11 +26,55 @@ function Weather() {
 
   const success = (position) => {
     const { latitude, longitude } = position.coords;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=018c97d6cc253f65fa3f407c8e0fc518`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=018c97d6cc253f65fa3f407c8e0fc518&units=metric`)
       .then(response => response.json())
-      .then(console.log);
+      .then(setWeather);
   };
-  navigator.geolocation.getCurrentPosition(success, console.log);
+  const error = (err) => { console.log(err); }
+  // navigator.geolocation.getCurrentPosition(success, console.log);
+
+  useEffect(() => {
+    // console.log("use EFFECT");  
+    navigator.geolocation.getCurrentPosition(success, error);
+  }, []);
+
+
+
+  var change = document.getElementsByTagName("body")[0];
+  useEffect(() => {
+    console.log(weather);
+    // console.log(weather !== 'undefined' ? 'weather is null or undefined' : "LENGTH: " + weather.weather.length);
+
+    if (Object.keys(weather).length === 0) {
+      console.log("hello");
+      return;
+    }
+
+    else {
+      console.log("pooohh");
+      // console.log("LENGTH: " + weather.weather.length)
+      if (weather.weather[0].main === 'Clouds') {
+        change.style.backgroundImage = "url('./Pics/snow.jpg')";
+        document.getElementsByTagName('body')[0].style.background = "url('./Pics/snow.jpg')";
+        // change.style.backgroundColor = "red";
+      }
+      /*  else if (weather.weather[0].main === 'Snow') {
+         document.getElementsByTagName('body')[0].style.backgroundImage = "url('./Pics/haze.jpg')";
+         // change.style.backgroundColor = "pink";
+       } */
+      else if (weather.weather[0].main === 'Clear') {
+        // document.body.style.backgroundColor = "#f7797d";
+        document.body.style.backgroundImage = "url('snow.jpg')";
+        // document.querySelector('body').style.background = "url('./Pics/clear.jpg')";
+      }
+    }
+
+  }, [weather]);
+
+
+
+
+
 
 
 
@@ -39,7 +82,7 @@ function Weather() {
     <div >
       <div className="row justify-content-center">
         <div className="heading ">
-          <h2> <img className="hat" src={Hat} />What's the Forecast ?? </h2>
+          <h2> <img className="hat" src={Hat} alt="hi" />What's the Forecast ?? </h2>
         </div>
 
         <div className="app-main container">
@@ -51,6 +94,7 @@ function Weather() {
               onChange={(e) => /* console.log(e.target.value) */ setval(e.target.value)} onKeyPress={search} />
           </div>
 
+          {/* if weather is true(means NOT NULL) tn only it will execute */}
           {weather.main && (
             <div className="weather-body">
               <div className="location-details">
@@ -63,7 +107,7 @@ function Weather() {
 
               <div className="weather-status">
                 <div className="temp" >
-                  {Math.round(weather.main.temp)}&deg;C
+                  {(weather.main.temp)}&deg;C          {/* Math.round*/}
                 </div>
                 <div className="min-max" >
                   {Math.floor(weather.main.temp_min)}<sup>&deg;C</sup>(min)/
